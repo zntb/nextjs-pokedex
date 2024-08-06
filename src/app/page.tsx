7e569a896ae5c23d3756141fee219974ea6deaf3
zntb <zenetibi@yahoo.com>
2024-08-06 13:03:47 +0300

    Add Custom SWR hook + error handling, pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,62 @@
-import Link from 'next/link';
+'use client';
+
+import PokemonEntry from '@/components/PokemonEntry';
+import { useRouter, useSearchParams, usePathname } from 'next/navigation';
+import useSWR from 'swr';
+import * as PokemonApi from '@/network/pokemon-api';
+import { Button, Col, Row, Spinner } from 'react-bootstrap';
 
 export default function Home() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+
+  const page = parseInt(searchParams.get('page') || '1');
+
+  const { data, error, isLoading } = useSWR(['getPokemonPage', page], () =>
+    PokemonApi.getPokemonPage(page),
+  );
+
+  if (isLoading)
+    return <Spinner animation="border" className="d-block m-auto" />;
+
+  if (error)
+    return (
+      <div>
+        <h1 className="text-center my-2">Failed to load data</h1>
+      </div>
+    );
+
   return (
     <div>
       <h1 className="text-center mb-4">Gotta cache &apos;em all</h1>
-      <Link href="/bulbasaur" className="link-light">
-        Bulbasaur
-      </Link>
+      <Row xs={1} sm={2} lg={3} xl={4} className="g-4">
+        {data?.results.map((pokemonEntry) => (
+          <Col key={pokemonEntry.name}>
+            <PokemonEntry name={pokemonEntry.name} />
+          </Col>
+        ))}
+      </Row>
+      <div className="d-flex justify-content-center gap-2 mt-4">
+        {data?.previous && (
+          <Button
+            onClick={() =>
+              router.push(pathname + '?page=' + (page - 1).toString())
+            }
+          >
+            Previous page
+          </Button>
+        )}
+        {data?.next && (
+          <Button
+            onClick={() =>
+              router.push(pathname + '?page=' + (page + 1).toString())
+            }
+          >
+            Next page
+          </Button>
+        )}
+      </div>
     </div>
   );
 }
--- src/components/PokemonDetails.tsx
+++ src/components/PokemonDetails.tsx
@@ -1,55 +1,50 @@
 'use client';
 
 import { useParams } from 'next/navigation';
-import useSWR from 'swr';
-import * as PokemonApi from '@/network/pokemon-api';
 import Link from 'next/link';
 import { Spinner } from 'react-bootstrap';
 import Image from 'next/image';
+import usePokemon from '@/hooks/usePokemon';
 
 export default function PokemonDetails() {
   const params = useParams();
 
   const pokemonName = params.pokemon as string;
 
-  const { data: pokemon, isLoading: pokemonLoading } = useSWR(
-    pokemonName,
-    PokemonApi.getPokemon,
-  );
+  const { pokemon, pokemonLoading } = usePokemon(pokemonName);
 
   return (
-    <>
-      <div className="d-flex flex-column align-items-center">
-        <p>
-          <Link href="/" className="link-light">
-            ← PokéDex
-          </Link>
-        </p>
-        {pokemonLoading && <Spinner animation="grow" />}
-        {pokemon && (
-          <>
-            <h1 className="text-center text-capitalize">{pokemon.name}</h1>
-            <Image
-              src={pokemon.sprites.other['official-artwork'].front_default}
-              alt={'Pokemon: ' + pokemon.name}
-              width={400}
-              height={400}
-            />
-            <div className="d-inline-block mt-2">
-              <div>
-                <strong>Types:</strong>{' '}
-                {pokemon.types.map((type) => type.type.name).join(', ')}
-              </div>
-              <div>
-                <strong>Height:</strong> {pokemon.height * 10} cm
-              </div>
-              <div>
-                <strong>Weight:</strong> {pokemon.weight / 10} kg
-              </div>
+    <div className="d-flex flex-column align-items-center">
+      <p>
+        <Link href="/" className="link-light">
+          ← PokéDex
+        </Link>
+      </p>
+      {pokemonLoading && <Spinner animation="grow" />}
+      {pokemon === null && <p>Pokemon not found</p>}
+      {pokemon && (
+        <>
+          <h1 className="text-center text-capitalize">{pokemon.name}</h1>
+          <Image
+            src={pokemon.sprites.other['official-artwork'].front_default}
+            alt={'Pokemon: ' + pokemon.name}
+            width={400}
+            height={400}
+          />
+          <div className="d-inline-block mt-2">
+            <div>
+              <strong>Types:</strong>{' '}
+              {pokemon.types.map((type) => type.type.name).join(', ')}
             </div>
-          </>
-        )}
-      </div>
-    </>
+            <div>
+              <strong>Height:</strong> {pokemon.height * 10} cm
+            </div>
+            <div>
+              <strong>Weight:</strong> {pokemon.weight / 10} kg
+            </div>
+          </div>
+        </>
+      )}
+    </div>
   );
 }
